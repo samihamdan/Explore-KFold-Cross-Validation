a615a2bacd8c1d1870cce2722029e30b10b55016
Sami Hamdan <sami.hamdan@hhu.de>
2020-03-23 12:47:32 +0100

    ADD fitting estimator in KFolds and transforming the input

--- ExploreKFold/computations.py
+++ ExploreKFold/computations.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+from sklearn.base import clone
 from sklearn.model_selection import KFold
+
 
 def data_generator(n, noise='normal', scale=.5, underlying_relation='linear'):
     x = np.linspace(start=0, stop=10, num=n)
@@ -38,3 +40,23 @@ def KFold_split(df_data, df_data_extra, n_splits, shuffle):
 
         df = pd.concat([df, pd.concat([df_fold, df_extra])])
     return df
+
+
+def fit_transform(df_splitted_data, estimator):
+    splits = df_splitted_data.data_split.unique()
+    estimators = [clone(estimator, safe=True) for _ in range(len(splits))]
+    for split, estimator in zip(splits, estimators):
+        df_split = df_splitted_data.loc[df_splitted_data['data_split'] == split]
+
+        df_train = df_split.loc[df_split['in_train_set'] == 'in_fold']
+        df_test = df_split.loc[df_split['in_train_set'] == 'out_fold']
+        df_extra_test = df_split.loc[df_split['in_train_set'] == 'extra_test']
+
+        estimator.fit(df_train.x.values.reshape(-1, 1), df_train.y)
+
+        for df, in_train_set in zip([df_train, df_test, df_extra_test], ['in_fold', 'out_fold', 'extra_test']):
+            y_pred = estimator.predict(df.x.values.reshape(-1, 1))
+            df_splitted_data.loc[(df_splitted_data['data_split'] == split) &
+                                 (df_splitted_data['in_train_set'] == in_train_set), 'y_pred'] = y_pred
+
+    return df_splitted_data
